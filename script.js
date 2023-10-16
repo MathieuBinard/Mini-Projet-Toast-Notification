cdc11bbbf0ab2ee769cba4431341fec709d754aa
MathieuBinard <93312097+MathieuBinard@users.noreply.github.com>
2023-10-16 20:27:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+//Je sélectionne et stocke le bouton
+const btn = document.querySelector('.btn');
+console.log(btn);
+//Je sélectionne et stocke la DIV container-notifications
+const ctn = document.querySelector('.container-notifications');
+console.log(ctn);
+
+btn.addEventListener('click', function(){
+    console.log('bouton cliqué');
+
+//Je crée un élément
+const notification = document.createElement('div');
+console.log(notification);
+//Je donne un style à la DIV
+notification.classList.add('toast');
+//J'insère du contenu texte
+notification.innerText = "Votre fichier est bien enregistré !"
+console.log(notification);
+//J'accroche notification à l'élément ctn
+ctn.appendChild(notification);
+    //A la fin du chrono, je rentre la notification du DOM
+    setTimeout(function(){
+        notification.remove();
+    }, 2000);
+});  
